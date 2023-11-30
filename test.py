02d24ad8154be6f16887459eb2c528d879e50ef3
avmonte <unstoppablehay@gmail.com>
2023-11-30 18:32:48 +0400

    the lat bug was the wrong calculation of exponents sue to apparent type incapataility of python's int and np.int

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 # This file is for testing purposes only
-# Let's implement the computation described in the provided text.
-print(' '.join([hex(i) for i in [179, 166, 219, 60, 135, 12, 62, 153, 36, 94, 13, 28, 6, 183, 71, 222]]))
+#
+print(' '.join([hex(i) for i in [243, 168, 141, 254, 190, 242, 235, 113, 255, 160, 208, 59, 117, 6, 140, 126, 135, 120, 115, 77, 208, 190, 130, 190, 219, 194, 70, 65, 43, 140, 250, 48]]))
 
 
-key = "29 23 be 84 e1 6c d6 ae 52 90 49 f1 f1 bb e9 eb"
+# key = "29 23 be 84 e1 6c d6 ae 52 90 49 f1 f1 bb e9 eb"
+#
+# plain = "b3 a6 db 3c 87 c 3e 99 24 5e d 1c 6 b7 47 de"
 
-plain = "b3 a6 db 3c 87 c 3e 99 24 5e d 1c 6 b7 47 de"
+# "48 d3 8f 75 e6 d9 1d 2a e5 c0 f7 2b 78 81 87 44 e 5f 50 0 d4 61 8d be"
+
+# "f3 a8 8d fe be f2 eb 71 ff a0 d0 3b 75 6 8c 7e 87 78 73 4d d0 be 82 be db c2 46 41 2b 8c fa 30"
