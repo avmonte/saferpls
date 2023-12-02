14ae7011343a77bbc8e13e461ffc9dbfc48080ac
avmonte <unstoppablehay@gmail.com>
2023-12-03 03:00:24 +0400

    major interface and format update

--- code/main.py
+++ code/main.py
@@ -1,31 +1,53 @@
+import sys
+import re
 import numpy as np
 from copy import deepcopy
 from time import time
-import sys
 
 from tools import *
 
 
-def check_format():
-	# TODO
-	pass
+def validate_format():
+	# python3 main.py [-e | -d] [file.txt] "[key]"
+	args = sys.argv
 
+	# Validate number of arguments
+	if len(args) != 4:
+		return 'Argument missing'
 
-def read_split(file):
-	# reading in binary
-	with open(file, "rb") as f:
+	# Validate the first argument (mode)
+	mode = args[1]
+	if not re.match(r'-e|-d', mode):
+		return 'Invalid mode'
+
+	# Validate and format the second argument (file)
+	with open(args[2], "rb") as f: # reading in binary
 		content = f.read()
 		no_bytes = len(content)
 
 		# Padding
 		off = no_bytes % 16
 		if off != 0:
-			content += (padding[off-1:]).encode()
+			content += (padding[off - 1:]).encode()
 
-		return [content[i:i + 16] for i in range(0, no_bytes, 16)]  # OPTIMIZE
+		blocks = [content[i:i + 16] for i in range(0, no_bytes, 16)]
+
+	# Validate and format the third argument (key)
+	# TODO: this be done through re
+	original_key = args[3]
+	length = len(original_key)
+	if all(i in '0123456789abcdefABCDEF' for i in original_key) and length in (32, 48, 64):
+		key = [int(original_key[i:i+2], 16) for i in range(0, length, 2)]
+		length //= 2
+	elif len(original_key) in (16, 24, 32):
+		key = [ord(i) for i in original_key]
+	else:
+		return 'Invalid key'
+
+	return blocks, key, length, mode
 
 
-def generate_key_schedule():
+def generate_key_schedule(key, key_length):
 	schedule = [key[:16]]  # list of subkeys from 1 to key length + 1 inclusive
 	# (each subkey is a string of the length of the key)
 
@@ -83,42 +105,43 @@ def reverse_step_four_arm(c):
 	return current
 
 
-def encrypt():
-	key_schedule = generate_key_schedule()
+def reverse_step_four_matrix(c):
+	# TODO
+	pass
 
+
+def encrypt(blocks, key_schedule, key_length):
 	cipher = []
 
 	for block in blocks:
 		current = deepcopy(block)  # create a block copy
 
 		# Encryption: Rounds
-		for i in range(r):
+		for i in range(key_length // 2):
 			current = correspond(current, key_schedule[2*i], '^++^' * 4)  # step 1/4
 			current = correspond(current, [1] * BLOCK_SIZE, 'elle' * 4)  # step 2/4
 			current = correspond(current, key_schedule[2*i+1], '+^^+' * 4)  # step 3/4
 			current = step_four_arm(current)  # step_four_matrix(current)
 
 		# Encryption: Last Step
-		current = correspond(current, key_schedule[2*r], '^++^' * 4)
+		current = correspond(current, key_schedule[key_length], '^++^' * 4)  # 2 * (key_length // 2)
 
 		cipher.append(current)  # Save manipulated block copy
 
 	return cipher
 
 
-def decrypt():
-	key_schedule = generate_key_schedule()
-
+def decrypt(blocks, key_schedule, key_length):
 	plain = []
 
 	for block in blocks:
 		current = deepcopy(block)  # create a block copy
 
 		# Encryption: Last Step
-		current = correspond(current, key_schedule[2 * r], '^--^' * 4)
+		current = correspond(current, key_schedule[key_length], '^--^' * 4)  # 2 * (key_length // 2)
 
 		# Encryption: Rounds
-		for i in range(r-1, -1, -1):
+		for i in range((key_length // 2) - 1, -1, -1):
 			current = reverse_step_four_arm(current)
 			current = correspond(current, key_schedule[2 * i + 1], '-^^-' * 4)
 			current = correspond(current, [1] * BLOCK_SIZE, 'leel' * 4)
@@ -129,6 +152,11 @@ def decrypt():
 	return plain
 
 
+def convert_back_to_original_format():
+	# TODO: incorporate remove_padding() into this function
+	pass
+
+
 def remove_padding(decrypted):  # TEST
 	for i in range(15, -1, -1):
 		if decrypted[i] != padding[i]:
@@ -136,32 +164,26 @@ def remove_padding(decrypted):  # TEST
 
 
 def main():
-	if key_length not in (16, 24, 32):
-		return 'Invalid key'
+	validation = validate_format()
+	if len(validation) == 1:
+		return validation
+	else:
+		blocks, key, key_length, mode = validation
+
+	key_schedule = generate_key_schedule(key, key_length)
 
 	print(f"\nKEY ---------> {np.array(key)}")
 	if mode == '-e':
 		print(f"PLAIN -------> {np.array(blocks)}")
 		print(f"CIPHER ------> ", end='')
-		return np.array(encrypt())
-	elif mode == '-d':
+		return np.array(encrypt(blocks, key_schedule, key_length))
+	else:
 		print(f"CIPHER -------> {np.array(blocks)}")
 		print(f"PLAIN ------> ", end='')
-		return decrypt()
-	else:
-		return 'Invalid mode'
+		return np.array(decrypt(blocks, key_schedule, key_length))
 
 
 start = time()
-# python3 main.py [-e | -d] [file.txt] "[key]"
-mode = sys.argv[1]
-original_file = sys.argv[2]
-original_key = sys.argv[3]
-
-blocks = read_split(original_file)
-key = [int(i, 16) for i in original_key.split()]
-key_length = len(key)
-r = key_length // 2  # number of rounds
 
 print(main(), '\n')
 print(f"RUNTIME ----> {time() - start}")
